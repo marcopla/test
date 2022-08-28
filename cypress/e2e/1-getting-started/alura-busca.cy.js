4fa7f2de218d66cb4f9d8ea49f8d4c1a93fbe8cd
Marco Pla <marco.pla@gmail.com>
2022-08-27 23:58:17 -0300

    starting project

--- cypress/e2e/1-getting-started/alura-busca.cy.js
+++ cypress/e2e/1-getting-started/alura-busca.cy.js
@@ -3,5 +3,12 @@ describe('alura busca cursos', () => {
     cy.visit('https://www.alura.com.br');
   });
 
-  it('buscar curso de java', () => {});
+  it('buscar curso de java', () => {
+    cy.get('#header-barraBusca-form-campoBusca').type('Java');
+    cy.get('.header-barraBusca-form-submit').click();
+    cy.get('h4.busca-resultado-nome').should(
+      'contain',
+      'Formação Java e Orientação a Objetos',
+    );
+  });
 });
