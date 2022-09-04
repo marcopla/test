3779a81cc1af29f899b0db0996570a76d3325a44
Marco Pla <marco.pla@gmail.com>
2022-09-03 23:52:02 -0300

    it "fazer login do usuario invalido"

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,19 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   projectId: 'qmdwo2',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+    },
+
+    reporter: 'mochawesome',
+    reporterOptions: {
+      reportDir: 'cypress/report/mochawesome-report',
+      overwrite: true,
+      html: true,
+      json: false,
+      timestamp: 'mmddyyyy_HHMMss',
     },
   },
 });
