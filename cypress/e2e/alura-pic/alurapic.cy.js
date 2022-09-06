a43c34c38f8da54018564c4301815c688b8ac0b6
Marco Pla <marco.pla@gmail.com>
2022-09-05 23:59:07 -0300

    caso teste

--- cypress/e2e/alura-pic/alurapic.cy.js
+++ cypress/e2e/alura-pic/alurapic.cy.js
@@ -51,4 +51,22 @@ describe('Login e registro de usuários alura pic', () => {
       expect(str).to.equal('Invalid user name or password');
     });
   });
+
+  it('caso teste', () => {
+    cy.get('input[formcontrolname="userName"]').type('jaqueline');
+    cy.get('input[formcontrolname="password"]').type('1234');
+    cy.get('button[type="submit"]').click();
+    cy.on('window:alert', (str) => {
+      expect(str).to.equal('Invalid user name or password');
+    });
+  });
+
+  it('fazer login do usuário inválido', () => {
+    cy.get('input[formcontrolname="userName"]').type('jaqueline');
+    cy.get('input[formcontrolname="password"]').type('1234');
+    cy.get('button[type="submit"]').click();
+    cy.on('window:alert', (str) => {
+      expect(str).to.equal('Invalid user name or password');
+    });
+  });
 });
