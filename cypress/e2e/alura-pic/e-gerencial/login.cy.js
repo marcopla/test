1033d2545b0d5dc4a4877228ce35d24cac6a8231
Marco Pla <marco.pla@gmail.com>
2022-10-12 23:57:20 -0300

    intercpt

--- cypress/e2e/alura-pic/e-gerencial/login.cy.js
+++ cypress/e2e/alura-pic/e-gerencial/login.cy.js
@@ -2,5 +2,13 @@ describe('', () => {
   beforeEach(() => {
     cy.visit();
   });
-  it('', () => {});
+  cy.intercept('POST', 'https://alura-fotos.herokuapp.com/user/login', {
+    statusCode: 400
+} ).as('stubPost')
+  })
+  it('', () => {
+    cy.login(Cypress.env('userName'), Cypress.env('password'))
+    cy.wait('@stubPost')
+    cy.contains('a', '(Logout)').should('be.visible');
+  });
 });
