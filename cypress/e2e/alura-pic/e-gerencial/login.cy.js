e2d023d2750dc43747f13bccb4ab270fb8e5a09a
Marco Pla <marco.pla@gmail.com>
2022-10-16 23:58:24 -0300

    teste login ib

--- cypress/e2e/alura-pic/e-gerencial/login.cy.js
+++ cypress/e2e/alura-pic/e-gerencial/login.cy.js
@@ -11,4 +11,12 @@ describe('teste e-gerencial', () => {
       'Login efetuado com sucesso',
     );
   });
+  it('teste de login IB', () => {
+    cy.get('#header-barraBusca-form-campoBusca').type('login');
+    cy.get('.header-barraBusca-form-submit').click();
+    cy.get('h4.busca-resultado-nome').should(
+      'contain',
+      'Login efetuado com sucesso',
+    );
+  });
 });
